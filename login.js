0f2e8afd8d83aa362b7743a47055a00f68670fa6
Ciddiee <64249420+Ciddiee@users.noreply.github.com>
2025-04-09 23:01:08 +0200

    Update login.js

--- login.js
+++ login.js
@@ -1,11 +1,11 @@
 document.getElementById('login-btn').addEventListener('click', function () {
     const clientId = '0e003d9184eb4010a520086c26a4b9e7';
-    const redirectUri = 'http://127.0.0.1:5500/callback.html';
+    const redirectUri = "https://ciddiee.github.io/DashboardMe/callback.html";
     const scope = 'user-top-read user-library-read playlist-read-private user-read-recently-played';
 
     const authUrl = `https://accounts.spotify.com/authorize` +
         `?client_id=${clientId}` +
-        `&response_type=token` + // 🔁 NYCKEL: Implicit flow = token direkt
+        `&response_type=token` +
         `&redirect_uri=${encodeURIComponent(redirectUri)}` +
         `&scope=${encodeURIComponent(scope)}` +
         `&show_dialog=true`;
